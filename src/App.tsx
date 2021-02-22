a5ccb1d3bdbb32109bb66175ae41cd5d8e2bce88
Arthuro Verissimo <arthurobdfv@gmail.com>
2021-02-22 20:32:45 +0100

    First Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import React from "react";
+import { ExperienceBar } from './components/Experiencebar'
+import './styles/global.css'
 
 function App() {
   return (
-    <h1> Hello World </h1>
+    <div className="container">
+      <ExperienceBar />
+    </div>
   );
 }
 
